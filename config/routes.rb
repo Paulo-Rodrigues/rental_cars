08dced9311d5f802512fa4666c8dd77c37a2e74c
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-10 17:31:27 -0300

    admin registra uma categoria de carro

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :subsidiaries, only: [:index, :show]
-  resources :car_categories, only: [:index, :show]
+  resources :car_categories, only: [:index, :show, :new, :create]
   root to: 'home#index'
 end
