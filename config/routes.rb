cf5060ffd81c43a4bd493cc5f20886fc13a34c39
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-10 17:55:02 -0300

    admin registra uma nova subsidiaria

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :subsidiaries, only: [:index, :show]
+  resources :subsidiaries, only: [:index, :show, :new, :create]
   resources :car_categories, only: [:index, :show, :new, :create]
   root to: 'home#index'
 end
