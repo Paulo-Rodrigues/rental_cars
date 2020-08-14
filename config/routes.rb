d08e51c581528a23ecea54669cc06ff0377f4715
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-14 00:05:29 -0300

    admin vê os modelos de carro

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :subsidiaries, only: [:index, :show, :new, :create, :edit, :update]
   resources :car_categories
+  resources :car_models, only: [:index]
   root to: 'home#index'
 end
---
+++ spec/features/car_models/admin_view_car_models_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Admin view car models', type: :feature do
+  scenario 'list of car models' do
+    car_category = CarCategory.create!(name: 'Economic', daily_rate: 105.5, car_insurance: 58.5,
+third_party_insurance: 10.5)
+    CarModel.create!(name: 'Uno', year: 1990, manufacturer: 'Fiat', motorization: '1.0', car_category_id: car_category.id, fuel_type: 'gasoline' )
+    CarModel.create!(name: 'Beatle', year: 1963, manufacturer: 'Volkswagen', motorization: '1.0', car_category_id: car_category.id, fuel_type: 'gasoline' )
+
+    visit root_path
+    click_on 'Modelos de carro'
+
+    expect(page).to have_content('Fiat')
+    expect(page).to have_content('Volkswagen')
+    expect(page).to have_content('Uno')
+    expect(page).to have_content('Beatle')
+    expect(page).to have_content('1963')
+    expect(page).to have_content('1990')
+  end
+end
