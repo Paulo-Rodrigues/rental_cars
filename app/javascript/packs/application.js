ac7e63cd713b27c5dca3aa83166e9c72b351dbd2
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-16 22:42:31 -0300

    algumas alterações de estilização

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("channels")
 
 import 'materialize-css/dist/js/materialize'
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
