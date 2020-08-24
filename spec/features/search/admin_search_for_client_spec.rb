4eb88d9a91bfcc1327d3ec5e26f118398de8a1ba
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-24 17:37:45 -0300

    (Search) criado links para as show pages dos respectivos objetos procurados

--- spec/features/search/admin_search_for_client_spec.rb
+++ spec/features/search/admin_search_for_client_spec.rb
@@ -26,6 +26,19 @@ feature 'Admin search for a client' do
     expect(page).to have_content(client1.cpf)
   end
 
+  scenario 'link_to show' do
+    user = create_user; login_as(user)
+    client1 = Client.create!(name: 'Fulano', cpf: CPF.new(CPF.generate).formatted , email: 'test1@test.com')
+    visit root_path
+
+    fill_in 'Procurar', with: client1.cpf
+    click_on 'Pesquisar'
+
+    click_on "#{client1.name} - #{client1.cpf}"
+
+    expect(current_path).to eq(client_path(client1))
+  end
+
   scenario 'client not found' do
     user = create_user; login_as(user)
     visit root_path
--- spec/features/search/admin_search_for_rental_code_spec.rb
+++ spec/features/search/admin_search_for_rental_code_spec.rb
@@ -37,4 +37,26 @@ feature 'Admin search for rental code(token)' do
     expect(page).to have_content(rental1.rental_code)
     expect(page).not_to have_content(rental2.rental_code)
   end
+
+  scenario 'link_to rental(show)' do
+    user = create_user; login_as(user)
+
+    car_category = CarCategory.create!(name: 'Economic', daily_rate: 100,
+                                       car_insurance: 100,
+                                       third_party_insurance: 100)
+    client = Client.create!(name: 'Cliente', cpf: '246.480.429-05',email: 'test@test.com')
+    rental1 = Rental.create!(start_date: '22/08/2030', end_date: '24/08/2030',
+                             car_category: car_category, client: client, user: user)
+
+
+    visit root_path
+
+    fill_in 'Procurar', with: rental1.rental_code
+
+    click_on 'Pesquisar'
+
+    click_on "Locação #{rental1.rental_code}"
+
+    expect(current_path).to eq(rental_path(rental1))
+  end
 end
